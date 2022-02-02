6de10440040ffb4631b4e662c4ae718b8d8be7b0
Kauê Jacyntho <kauekaj@gmail.com>
2022-02-02 07:27:42 -0300

    add debugError

---
+++ debugError/debugError.js
@@ -0,0 +1,30 @@
+function validaArray(arr, num) {
+  try {
+    if (!arr && !num) throw new ReferenceError("Envie os parâmetros.");
+
+    if (typeof arr !== "object")
+      throw new TypeError("Array precisa ser do tipo object.");
+
+    if (typeof num !== "number")
+      throw new TypeError("Num precisa ser do tipo number.");
+
+    if (arr.length !== num) throw new RangeError("Tamanho inválido.");
+
+    return arr;
+  } catch (error) {
+    if (error instanceof ReferenceError) {
+      console.log("Este erro é um ReferenceError");
+      console.log(error.message);
+    } else if (error instanceof TypeError) {
+      console.log("Este erro é um TypeError");
+      console.log(error.message);
+    } else if (error instanceof RangeError) {
+      console.log("Este erro é um RangeError");
+      console.log(error.message);
+    } else {
+      console.log("Tipo de erro não esperado:" + error);
+    }
+  }
+}
+
+console.log(validaArray([1, 5, 6], 3));
